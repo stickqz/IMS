fa60ccc7a8ee7e3b13ea45da74c1ba2971aa70ad
Anuj Yadav <anujyadavvvv12345@gmail.com>
2023-08-15 00:45:40 +0530

    signin page added

---
+++ src/components/signin1/signin.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import './signin.css';
+
+function Signup() {
+  const [activeTab, setActiveTab] = useState('loginAdmin');
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    if (activeTab === 'loginAdmin') {
+      console.log(' ');
+    } else if (activeTab === 'loginUser') {
+      console.log(' ');
+    }
+  };
+
+  return (
+    <div className='container'>
+      <div className='firsthalf'>
+        <h1> Login to Your Account</h1>
+        <div className='tabs'>
+          <div className='tab-label'>Admin</div>
+          <label
+            className={`switch ${activeTab === 'loginUser' ? 'user' : 'admin'}`}
+          >
+            <input
+              type='checkbox'
+              onChange={() =>
+                handleTabChange(
+                  activeTab === 'loginAdmin' ? 'loginUser' : 'loginAdmin'
+                )
+              }
+            />
+            <span className='slider'></span>
+          </label>
+          <div className='tab-label'>User</div>
+        </div>
+        <div className='login-form'>
+          <form onSubmit={handleLogin}>
+            <label className='name-field'>Username :</label>
+            <input type='text' placeholder='Enter Username' />
+            <br />
+            <label className='name-field'>Password :</label>
+            <input type='password' placeholder='Enter Password' />
+            <br />
+            <button type='submit' className='login-button'>
+              {activeTab === 'loginAdmin' ? 'Login As Admin' : 'Login As User'}
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className='secondhalf'>
+            <h3>New Here?</h3>
+             <p>Sign Up and discover a great
+             <br/> amount of new Opportunities!</p>
+            <div className='button-container'>
+         <button
+           className={`create-account-button ${activeTab === 'newAdmin' ? 'active' : ''}`}
+           onClick={() => handleTabChange('newAdmin')}>
+            Create Account
+         </button>
+        </div>
+    </div>
+    </div>
+  );
+}
+
+export default Signup;
--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,71 +1,49 @@
-import React, { useState } from 'react';
-import './signup.css';
+import React from 'react'
+import './signup.css'
 
-function Signup() {
-  const [activeTab, setActiveTab] = useState('loginAdmin');
-
-  const handleTabChange = (tab) => {
-    setActiveTab(tab);
-  };
-
-  const handleLogin = (event) => {
-    event.preventDefault();
-    if (activeTab === 'loginAdmin') {
-      console.log(' ');
-    } else if (activeTab === 'loginUser') {
-      console.log(' ');
-    }
-  };
-
+function signup() {
   return (
     <div className='container'>
-      <div className='firsthalf'>
-        <h1> Login to Your Account</h1>
-        <div className='tabs'>
-          <div className='tab-label'>Admin</div>
-          <label
-            className={`switch ${activeTab === 'loginUser' ? 'user' : 'admin'}`}
-          >
-            <input
-              type='checkbox'
-              onChange={() =>
-                handleTabChange(
-                  activeTab === 'loginAdmin' ? 'loginUser' : 'loginAdmin'
-                )
-              }
-            />
-            <span className='slider'></span>
-          </label>
-          <div className='tab-label'>User</div>
-        </div>
-        <div className='login-form'>
-          <form onSubmit={handleLogin}>
-            <label className='name-field'>Username :</label>
-            <input type='text' placeholder='Enter Username' />
-            <br />
-            <label className='name-field'>Password :</label>
-            <input type='password' placeholder='Enter Password' />
-            <br />
-            <button type='submit' className='login-button'>
-              {activeTab === 'loginAdmin' ? 'Login As Admin' : 'Login As User'}
-            </button>
-          </form>
-        </div>
-      </div>
-      <div className='secondhalf'>
-            <h3>New Here?</h3>
-             <p>Sign Up and discover a great
-             <br/> amount of new Opportunities!</p>
+        <div className='firsthalf'>
+        <h3>One of Us?</h3>
+             <p>If you already have an account,
+             <br/> just sign in. we've missed you!</p>
             <div className='button-container'>
          <button
-           className={`create-account-button ${activeTab === 'newAdmin' ? 'active' : ''}`}
-           onClick={() => handleTabChange('newAdmin')}>
-            Create Account
+           className={`create-account-button `}>
+            sign in
          </button>
+         </div>
+
         </div>
+        <div className='secondhalf'>
+           <h1>Create Free Account</h1>
+           <div className='signup-form'>
+           <form >
+                   <label className='name-field'>Username :</label>
+                   <input type='text' placeholder='Enter Username' />
+                <br/>
+                <label className='name-field'>Email :</label>
+                <input type='email' placeholder='Enter Email' />
+                <br />
+                <label className='name-field'>Password :</label>
+                   <input type='password' placeholder='Enter Password' />
+               <br />
+               
+                 <div className='button-container'>
+               <button type='submit' className='create-account-button'>Sign Up
+            </button>
+            </div>
+            </form>
+            </div>
+               
+               
+          
+        
+        </div>
+
     </div>
-    </div>
-  );
+  )
 }
 
-export default Signup;
+export default signup
