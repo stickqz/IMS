6732bf95ed067b817931e4def7fe1af5e3b3f6e9
stickqz <rkmehta246@gmail.com>
2023-08-19 17:35:39 +0530

    partly merge dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./components/Form/Register.js";
 import LandingPage from "./components/Landing/LandingPage.js";
-import Dashboard from "./components/Dashboard";
+import Dashboard from "./components/Dashboard/Dashboard.js";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/register" element={<Register />} />
-        <Route element={<PrivateRoute />}>
-          <Route element={<Dashboard />} path="/dashboard" />
-        </Route>
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/register" element={<Register />} />
+				<Route element={<PrivateRoute />}>
+					<Route element={<Dashboard />} path="/dashboard" />
+				</Route>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,13 +1,60 @@
-import Header from "../Header/Header";
-import Card from "../UI/Card/Card";
+// Dashboard.js
 
-const Dashboard = () => {
-	return (
-		<>
-			<Header />
-			<Card />
-		</>
-	);
-};
+import React, { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
+import axios from "axios";
+
+function Dashboard() {
+  const [userData, setUserData] = useState(null);
+  const token = localStorage.getItem("token"); // Retrieve token from storage
+  console.log("Token exists:", token);
+
+  useEffect(() => {
+    if (token) {
+      const decoded = jwt_decode(token);
+      const role = decoded.role;
+      console.log("Decoded Token exists:", decoded);
+
+      axios
+        .get(`http://localhost:5000/api/${role}/profile`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          setUserData(response.data);
+        })
+        .catch((error) => {
+          // Handle different types of errors
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            console.error("Error response from server:", error.response);
+          } else if (error.request) {
+            // The request was made but no response was received
+            console.error("No response from server:", error.request);
+          } else {
+            // Something happened in setting up the request
+            console.error("Request setup error:", error.message);
+          }
+
+          localStorage.removeItem("token");
+        });
+    }
+  }, [token]);
+
+  return (
+    <div>
+      {userData ? (
+        <div>
+          <h2>Welcome to Your Dashboard, {userData.username}</h2>
+          <p>Email: {userData.email}</p>
+          {/* Display other user-specific information */}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
 
 export default Dashboard;
