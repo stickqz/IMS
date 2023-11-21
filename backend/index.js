2789f7b9c4f1b79cb249970e29d078979155cd26
stickqz <rkmehta246@gmail.com>
2023-11-21 08:08:49 +0530

    update for deployment

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,12 @@ const MONGO_URI = process.env.MONGO_URI;
 
 app.use(express.json());
 
+app.use(
+	cors({
+		origin: "https://ims-client-delta.vercel.app",
+	})
+);
+
 mongoose
 	.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => {
